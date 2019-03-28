ffb2429e0188042c8297702d142e1aa1bfd4aa89
Thiswinex <zhou-bh16@mails.tsinghua.edu.cn>
2019-03-29 00:15:47 +0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,66 @@
+import os, json
+
+# Data input:
+file = open("./homework/pinyin_dict/pinyin_dict_utf.txt", encoding="utf-8")
+
+# Define the variable:
+strs = file.readlines()
+file.close()
+py_dict = {}
+data_dict_list = []
+counter1_dict = {} # 1 character freq
+counter2_dict = {} # 2 character freq
+counter_total = 0 # total freq
+not_count_char = "!@#$%^&*()！……「」【】{}|\:：;\"“”（）。，；,.1234567890《》<>?？/、~` qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+
+
+# News data input:
+data_path = "./homework/sina_news_utf8/"
+data_path_list = os.listdir(data_path)
+for i in range(len(data_path_list)):
+    file = open(data_path + data_path_list[i], encoding="utf-8")
+    #file = open(data_path+"test.txt", encoding="utf-8")
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        data_dict_list.append(json.loads(line))
+
+# --- Counting ---
+for i in range(len(data_dict_list)):
+    string = data_dict_list[i]['html']
+    for j in range(len(string) - 1):
+        # Ignore the character not Chinese
+        # Counting 1-gram
+        if string[j] in not_count_char:
+            continue
+        if string[j] not in not_count_char:
+            if string[j] not in counter1_dict:
+                counter1_dict.update({string[j]: 1})
+                counter_total += 1
+            else:
+                counter1_dict[string[j]] += 1
+                counter_total += 1
+        # counter1_dict's type: { char1 : value , char2 : value}
+
+        # Counting 2-gram
+        if string[j+1] in not_count_char:
+            j += 1
+            continue
+        if string[j] not in counter2_dict:
+            counter2_dict.update({string[j]: {string[j+1]: 1}})
+        elif string[j+1] not in counter2_dict[string[j]]:
+            counter2_dict[string[j]].update({string[j+1]: 1})
+        else:
+            counter2_dict[string[j]][string[j+1]] += 1
+        # counter2_dict's type: { char_a1: { char_b1: value, char_b2: value }, char_a2: ... }
+
+file1 = open("./counter1_dict.txt", "w", encoding="utf-8")
+file2 = open("./counter2_dict.txt", "w", encoding="utf-8")
+file1.write(json.dumps(counter1_dict, ensure_ascii=False))
+file2.write(json.dumps(counter2_dict, ensure_ascii=False))
+
+
+print(counter_total)
+
+
---
+++ main.py
@@ -0,0 +1,106 @@
+import os, json
+
+
+# Data input:
+file = open("./homework/pinyin_dict/pinyin_dict_utf.txt", encoding="utf-8")
+
+# Define the variable:
+strs = file.readlines()
+file.close()
+py_dict = {}
+file1 = open("./counter1_dict.txt", encoding="utf-8")
+file2 = open("./counter2_dict.txt", encoding="utf-8")
+counter1_dict = json.loads(file1.readline()) # 1 character freq
+counter2_dict = json.loads(file2.readline()) # 2 character freq
+counter_total = 396639694
+lam = 0.8 # define the lambda for smoothing algorithm
+
+
+# Pinyin dict input:
+for i in range(len(strs)):
+    key_n_value = strs[i].split(' ', 1)
+    py_dict.update({key_n_value[0]: key_n_value[1][:-1]})
+
+#  --- Input transfer ---
+input = "chen si yuan"
+input_spilt = input.split(" ")
+print(input_spilt)
+
+graph = []
+
+# Build the graph:
+for i in range(len(input_spilt)):
+    graph_list = []
+    for j in range(0, len( py_dict[input_spilt[i]] ), 2):
+        graph_list.append( {'char':py_dict[input_spilt[i]][j], 'value': 0, 'pre_dict': None})
+    graph.append(graph_list)
+    # graph's type: graph[layers][char_num] = {'char': char, 'value': freq, 'pre_dict': dict}
+
+
+def cal_cond_prob(char_a, char_b): # use frequency to replace probability
+    cond_prob = 0
+    try:
+        prob_ab = counter2_dict[char_a][char_b] / counter_total
+    except KeyError:
+        prob_ab = 0
+    try:
+        prob_a = counter1_dict[char_a] / counter_total
+    except KeyError:
+        prob_a = 0
+        cond_prob = 0
+    else:
+        cond_prob = prob_ab / prob_a
+    char_prob = lam * cond_prob + (1 - lam) * prob_a
+    return char_prob
+
+
+def find_ans():
+    for layer in range(len(graph)):
+        if layer == 0:
+            for dict in graph[layer]:
+                if dict['char'] in counter1_dict:
+                    dict['value'] = counter1_dict[dict['char']] / counter_total
+        else:
+            for dict in graph[layer]:
+                max_value = 0
+                pre_dict = None
+                for dict_pre in graph[layer - 1]:
+                    new_value = dict_pre['value'] * cal_cond_prob(dict_pre['char'], dict['char'])
+                    if new_value > max_value:
+                        max_value = new_value
+                        pre_dict = dict_pre
+                dict['value'] = max_value
+                dict['pre_dict'] = pre_dict
+
+    max_dict = None
+    max_value = 0
+    for i in range(len(graph[len(graph)-1])):
+        if graph[len(graph)-1][i]['value'] > max_value:
+            max_value = graph[len(graph)-1][i]['value']
+            max_dict = graph[len(graph)-1][i]
+
+    ans = ""
+    while True:
+        ans += max_dict['char']
+        if not max_dict['pre_dict']:
+            break
+        max_dict = max_dict['pre_dict']
+    l = list(ans)
+    l.reverse()
+    ans = "".join(l)
+    print(ans)
+    #print(graph)
+
+
+
+find_ans()
+
+
+'''
+for i in range(99):
+    print(str(lam)+"  ", end='')
+    find_ans()
+    lam += 0.01
+'''
+
+
